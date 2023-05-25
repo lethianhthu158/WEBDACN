c890d4ce19185aa703e27ed62f4dedbd421c817c
ANH THƯ <20521986@gm.uit.edu.vn>
2023-05-25 19:51:14 +0700

    Fix-login-popup

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,57 +1,74 @@
 import "./header.css";
-import logoImage from '../../assets/logo.png';
-import logoName from '../../assets/name.png'
+import logoImage from "../../assets/logo.png";
+import logoName from "../../assets/name.png";
 import React, { useState } from "react";
-// import {Dialog} from '@material-ui/core';
-import Popup from "../popup/popup";
 import Login from "../login/loginform";
 import Register from "../register/registerform";
+import { Dialog } from "@material-ui/core";
 
+function Header(props) {
+  const [openPopupLogin, setOpenPopupLogin] = useState(false);
+  const [openPopupRegister, setOpenPopupRegister] = useState(false);
 
-
-
-function Header  (props) {
-    const [openPopupLogin,setOpenPopupLogin]=useState(false);
-    const [openPopupRegister,setOpenPopupRegister]=useState(false);
-    
-    
-    return (
-        <div className={props.className}>
-        <div className="wrapHead">
-            <div className="headerPart1">
-                <img className="logoImage" src={logoImage} alt="Logo store" />
-                <img className="logoName"  src={logoName} alt="Logo name"></img>
-                <div className="wrapButtons">
-                    <button className="IconButton heartButton"><i class="icon fas fa-heart"></i></button>
-                    <button className="IconButton cartButton"><i class="icon fas fa-shopping-cart"></i></button>
-                    <button className="RegisterButton" onClick={()=> setOpenPopupRegister(true)} >Register</button>
-                    <button className="LoginButton" onClick={()=> setOpenPopupLogin(true)}>Login</button>
-                </div>
-            </div>
-            <div className="headerPart2">
-                <div className="control left"><button className="Tab Home">HOME</button><hr className="tap-control"></hr></div>
-                <div className="control"><button  className="Tab Product">PRODUCTS</button></div>
-                <div className="control"><button  className="Tab DisChannel">DISTRIBUTION CHANNEL</button></div>
-                <div className="control"><button  className="Tab About">ABOUT US</button></div>
-                <div className="control right">
-                    <input className="input-search"></input>
-                    <button className="searchButton"><i class="fas fa-search"></i></button>
-                </div>
-            </div>
-              <Popup
-             openPopup={openPopupLogin}
-             setOpenPopup={setOpenPopupLogin}>
-                <Login/>
-                
-            </Popup>  
-            <Popup
-             openPopup={openPopupRegister}
-             setOpenPopup={setOpenPopupRegister}>
-               <Register/>
-                
-            </Popup> 
+  return (
+    <div className={props.className}>
+      <div className="wrapHead">
+        <div className="headerPart1">
+          <img className="logoImage" src={logoImage} alt="Logo store" />
+          <img className="logoName" src={logoName} alt="Logo name"></img>
+          <div className="wrapButtons">
+            <button className="IconButton heartButton">
+              <i class="icon fas fa-heart"></i>
+            </button>
+            <button className="IconButton cartButton">
+              <i class="icon fas fa-shopping-cart"></i>
+            </button>
+            <button
+              className="RegisterButton"
+              onClick={() => setOpenPopupRegister(true)}
+            >
+              Register
+            </button>
+            <button
+              className="LoginButton"
+              onClick={() => setOpenPopupLogin(true)}
+            >
+              Login
+            </button>
+          </div>
         </div>
+        <div className="headerPart2">
+          <div className="control left">
+            <button className="Tab Home">HOME</button>
+            <hr className="tap-control"></hr>
+          </div>
+          <div className="control">
+            <button className="Tab Product">PRODUCTS</button>
+          </div>
+          <div className="control">
+            <button className="Tab DisChannel">DISTRIBUTION CHANNEL</button>
+          </div>
+          <div className="control">
+            <button className="Tab About">ABOUT US</button>
+          </div>
+          <div className="control right">
+            <input className="input-search"></input>
+            <button className="searchButton">
+              <i class="fas fa-search"></i>
+            </button>
+          </div>
         </div>
-    );
+        <Dialog open={openPopupLogin} onClose={() => setOpenPopupLogin(false)}>
+          <Login onClose={() => setOpenPopupLogin(false)} />
+        </Dialog>
+        <Dialog
+          open={openPopupRegister}
+          onClose={() => setOpenPopupRegister(false)}
+        >
+          <Register onClose={() => setOpenPopupRegister(false)} />
+        </Dialog>
+      </div>
+    </div>
+  );
 }
 export default Header;
--- src/components/login/loginform.jsx
+++ src/components/login/loginform.jsx
@@ -1,40 +1,52 @@
 import "./loginform.css";
-import { Link } from "@material-ui/core";
 
-
-
-
-function Login  () {
-   
-    
-    return (
+const Login = ({ onClose }) => {
+  return (
     <div className="wrap-loginform">
-        <div className="wrap-close"><Link to='/home'><button className="close-button" ><i class="far fa-times-circle"></i>
-        </button></Link></div>
-       
-        <p className="title">Login</p>
-        <div className="container-input">
-            <p className="title-input"> Email</p>
-            <input className="input"></input>
-            <p className="title-input"> Password</p>
-            <input className="input"></input>
-            <div className="wrap-fotgot"><a className="forgot-pass" href="https://www.facebook.com/profile.php?id=100077535672034">Forgot Password?</a></div>
+      <div className="wrap-close">
+        <button onClick={onClose} className="close-button">
+          <i class="far fa-times-circle"></i>
+        </button>
+      </div>
 
+      <p className="title">Login</p>
+      <div className="container-input">
+        <p className="title-input"> Email</p>
+        <input className="input"></input>
+        <p className="title-input"> Password</p>
+        <input className="input"></input>
+        <div className="wrap-fotgot">
+          <a
+            className="forgot-pass"
+            href="https://www.facebook.com/profile.php?id=100077535672034"
+          >
+            Forgot Password?
+          </a>
         </div>
-        <div className="wrap-icon">
-        <button className="accout-button"><i class="Icon fab fa-facebook-f"></i></button>
-        <button className="accout-button"><i class="Icon fab fa-google"></i></button>
+      </div>
+      <div className="wrap-icon">
+        <button className="accout-button">
+          <i class="Icon fab fa-facebook-f"></i>
+        </button>
+        <button className="accout-button">
+          <i class="Icon fab fa-google"></i>
+        </button>
+      </div>
+
+      <div className="wrap-login">
+        <div className="text-Resgiter">
+          New Here?{" "}
+          <a
+            className="forgot-pass"
+            href="https://www.facebook.com/profile.php?id=100077535672034"
+          >
+            Resgiter
+          </a>
         </div>
-        
-        <div className="wrap-login">
-            <div className="text-Resgiter">New Here? <a className="forgot-pass" href="https://www.facebook.com/profile.php?id=100077535672034">Resgiter</a></div>
-            <button className="login-button">
-                Login
-               
-            </button>
-        </div>
-        
+        <button className="login-button">Login</button>
+      </div>
     </div>
-    );
-}
+  );
+};
+
 export default Login;
