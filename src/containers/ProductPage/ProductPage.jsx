deb3d4994405504e9bde7e696454c36e01efc839
TinUIT <20522017@gm.uit.edu.vn>
2023-06-21 21:47:42 +0700

    connectApiFilter

--- src/containers/ProductPage/ProductPage.jsx
+++ src/containers/ProductPage/ProductPage.jsx
@@ -11,23 +11,39 @@ import axios from "axios";
 
 const ProductPage = () => {
     const [products, setProducts] = useState([]);
-    const [currentPage, setCurrentPage] = useState(0); 
-    const [totalPages, setTotalPages] = useState(0); // Add this line
-
+    const [currentPage, setCurrentPage] = useState(0);
+    const [totalPages, setTotalPages] = useState(0);
+    const [apiEndpoint, setApiEndpoint] = useState("http://localhost:8080/api/products");
     useEffect(() => {
-        axios.get(`http://localhost:8080/api/products?page=${currentPage}&size=4`)
+        axios.get(`${apiEndpoint}?page=${currentPage}&size=4`)
             .then(response => {
                 setProducts(response.data.content);
-                setTotalPages(response.data.totalPages); // Set the total number of pages
+                setTotalPages(response.data.totalPages);
             })
             .catch(error => {
                 console.error('There was an error!', error);
             });
-    }, [currentPage]);
+    }, [currentPage, apiEndpoint]);
     const [selectedButton, setSelectedButton] = useState(null);
     const [product, setProduct] = useState(0);
     const handleButtonClick = (buttonName) => {
         setSelectedButton(buttonName);
+        switch (buttonName) {
+            case 'Best-seller':
+                setApiEndpoint("http://localhost:8080/api/products/best-sellers");
+                break;
+            case 'Discount':
+                setApiEndpoint("http://localhost:8080/api/products/sales");
+                break;
+            case 'Low-price':
+                setApiEndpoint("http://localhost:8080/api/products/lowPrice");
+                break;
+            case 'High-price':
+                setApiEndpoint("http://localhost:8080/api/products/highPrice");
+                break;
+            default:
+                setApiEndpoint("http://localhost:8080/api/products");
+        }
     };
     const handleSelectProduct = (selectedIndexed, e) => {
         setProduct(selectedIndexed);
@@ -109,32 +125,32 @@ const ProductPage = () => {
             </div>
             <div className="Wrapper-Product-detail">
                 {products.map(product => (
-                    <Productdetail nameProduct={product.name} price={product.price} image={product.image}/>
+                    <Productdetail nameProduct={product.name} price={product.price} image={product.image} />
                 ))}
             </div>
 
 
             <div className="Wrapper-Pagniation">
-    <nav aria-label="Page navigation example">
-        <ul className="pagination">
-            <li className="page-item">
-                <a className="page-link" href="#" aria-label="Previous" onClick={() => handlePageChange(currentPage > 0 ? currentPage - 1 : 0)}>
-                    <span aria-hidden="true">&laquo;</span>
-                </a>
-            </li>
-            {pageNumbers.map(number => (
-                <li className={`page-item ${currentPage === number ? 'active' : ''}`} key={number}>
-                    <a className="page-link" href="#" onClick={() => handlePageChange(number)}>{number + 1}</a>
-                </li>
-            ))}
-            <li className="page-item">
-                <a className="page-link" href="#" aria-label="Next" onClick={() => handlePageChange(currentPage + 1 < totalPages ? currentPage + 1 : totalPages - 1)}>
-                    <span aria-hidden="true">&raquo;</span>
-                </a>
-            </li>
-        </ul>
-    </nav>
-</div>
+                <nav aria-label="Page navigation example">
+                    <ul className="pagination">
+                        <li className="page-item">
+                            <a className="page-link" href="#" aria-label="Previous" onClick={() => handlePageChange(currentPage > 0 ? currentPage - 1 : 0)}>
+                                <span aria-hidden="true">&laquo;</span>
+                            </a>
+                        </li>
+                        {pageNumbers.map(number => (
+                            <li className={`page-item ${currentPage === number ? 'active' : ''}`} key={number}>
+                                <a className="page-link" href="#" onClick={() => handlePageChange(number)}>{number + 1}</a>
+                            </li>
+                        ))}
+                        <li className="page-item">
+                            <a className="page-link" href="#" aria-label="Next" onClick={() => handlePageChange(currentPage + 1 < totalPages ? currentPage + 1 : totalPages - 1)}>
+                                <span aria-hidden="true">&raquo;</span>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
 
             <Footer />
         </>
